b82b37578e407010b9c375db39fdc097118ffb72
Unknown <aravindbk92@gmail.com>
2018-07-30 22:04:17 -0400

    Added parallel trial runs

--- test.py
+++ test.py
@@ -6,14 +6,5 @@ import math
 AVERAGED_OVER = 20
 
 max_num_episodes_trial = 366-AVERAGED_OVER
-episode_reward_trials = np.load("plotted_values/evoreward_pop20_rewards.npy")
-episode_reward_trials = episode_reward_trials[:,:max_num_episodes_trial]
-mean_rewards = np.average(episode_reward_trials,axis=0)    
-std_rewards = np.std(episode_reward_trials,axis=0)
-
-plt.errorbar(range(1,mean_rewards.size+1),mean_rewards,yerr=std_rewards,label="DQN-evoReward, pop: "+str(20))
-
-plt.xlabel("Episode")
-plt.ylabel("Average Cumulative Reward")
-plt.legend(title="DQN type")
-plt.savefig("test.png")
+episode_reward_trials = np.load("plotted_values/grid_evoreward_pop10MiniGrid-Empty-6x6-v01.npy")
+print (episode_reward_trials)
