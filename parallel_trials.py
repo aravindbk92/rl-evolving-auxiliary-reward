3cf11ec9d03f2e0ec83db20c3c7e15b22ed3519d
Unknown <aravindbk92@gmail.com>
2018-09-18 17:14:34 -0400

    Fixed bug in parallel trials

--- parallel_trials.py
+++ parallel_trials.py
@@ -38,11 +38,11 @@ def run_dqn_trials(env_id="MiniGrid-Empty-6x6-v0",
                    num_population=20):
 
     # Log and stat file names
-    trial_save_filename = "plotted_values/" + base_filename + 'maxrewards.npy'
-    bestagent_save_filename = "plotted_values/" + base_filename + 'agentrewards.npy'
+    base_filename = base_filename + env_id + str(dqn_type)
     base_filename = ('grid_evoreward_pop' + str(num_population)
                      if dqn_type == EVODQN_TYPE else 'grid_dqn')
-    base_filename = base_filename + env_id + str(dqn_type)
+    trial_save_filename = "plotted_values/" + base_filename + 'maxrewards.npy'
+    bestagent_save_filename = "plotted_values/" + base_filename + 'agentrewards.npy'
     log_filename = "logs/" + base_filename
 
     # initial vars
